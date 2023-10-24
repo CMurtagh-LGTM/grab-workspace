51cd5d1cc39385988db5f65e94ff4177f22dc9fb
cameron <cameron.murtagh00@gmail.com>
2023-10-24 15:41:21 +1100

    bit a bit more respecting of window rules

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <hyprland/src/helpers/Workspace.hpp>
 #include <hyprland/src/plugins/PluginAPI.hpp>
+#include <hyprland/src/config/ConfigManager.hpp>
 
 #include "globals.hpp"
 
@@ -17,13 +18,22 @@ void grab_workspace(std::string args){
         return;
     }
 
+    if (g_pConfigManager->getWorkspaceRuleFor(workspace_to_change_to).monitor != ""){
+        // We don't want to mess with workspaces with rules
+        HyprlandAPI::invokeHyprctlCommand("dispatch", "workspace " + args);
+        return;
+    }
+
     const auto old_monitor = g_pCompositor->getMonitorFromID(workspace_to_change_to->m_iMonitorID);
     const auto new_monitor = g_pCompositor->m_pLastMonitor;
 
-    if(old_monitor->activeWorkspace == workspace_to_change_to->m_iID){
+    if(
+        old_monitor->activeWorkspace == workspace_to_change_to->m_iID && // Moving from a workspace that is active on a screen
+        g_pConfigManager->getWorkspaceRuleFor(g_pCompositor->getWorkspaceByID(new_monitor->activeWorkspace)).monitor == "" // We don't want to swap with a workspace that has a monitor rule
+    ){
         // Swap the workspaces
         HyprlandAPI::invokeHyprctlCommand("dispatch", "swapactiveworkspaces " + old_monitor->szName + " " + new_monitor->szName);
-    }else{
+    } else {
         // Grab the workspace
         HyprlandAPI::invokeHyprctlCommand("dispatch", "moveworkspacetomonitor " + workspace_to_change_to->m_szName + " current");
         HyprlandAPI::invokeHyprctlCommand("dispatch", "workspace " + workspace_to_change_to->m_szName);
@@ -41,6 +51,6 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 
     HyprlandAPI::addDispatcher(PHANDLE, "grab-workspace", grab_workspace);
 
-    return {"grab-workspace", "Grabs a workspace", "CMurtagh-LGTM", "0.0"};
+    return {"grab-workspace", "Grabs a workspace", "CMurtagh-LGTM", "0.1"};
 }
 
